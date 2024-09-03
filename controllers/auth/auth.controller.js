4d2c3a51ab9e16fc7b314fe0d1ddb1d606c31562
xeetriaman <77188934+xeetriaman@users.noreply.github.com>
2024-09-03 12:47:24 +0545

    Add .gitignore file

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -1,52 +1,52 @@
-import bcrypt from "bcryptjs/dist/bcrypt.js"
-import jwt from "jsonwebtoken"
-import {User} from "../../models/index.js"    
-import { showError } from "../../lib/index.js"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { User } from "../../models/index.js";
+import { showError } from "../../lib/index.js";
 
-class Authcontroller{
-   
-    register = async (req,res,next)=>{
-        try{
-            const {name,email,password,phone,address}=req.body
-            const hash =bcrypt.hashSync(password,bcrypt.genSaltSync(12))
-            await User.create({name,email,password:hash,phone,address})
+class AuthController {
+    register = async (req, res, next) => {
+        try {
+            const { name, email, password, phone, address } = req.body;
+            const hash = bcrypt.hashSync(password, bcrypt.genSaltSync(12));
+            await User.create({ name, email, password: hash, phone, address });
             res.status(201).json({
-                sucess:'Thank you for registering with us.',
-            })
-        }catch(e){
-            showError(e,next)
+                success: 'Thank you for registering with us.'
+            });
+        } catch (error) {
+            showError(error, next);
         }
+    };
 
-    }
-    
-    login= async (req,res,next)=>{
-        try{
-            const{email,password}=req.body
-            const user = await User.findOne({email}).exec()
-                if (user!=null){
-                  if(bcrypt.compareSync(password,user.password)){
-                    const token =jwt.sign({
-                        data:user._id,
-                        iat:Math.floor(Date.now()/1000),
-                        exp:Math.floor(Date.now()/1000+30*24*60*60),
-
-                    },process.env.JWT_SECRET)
-                    res.json({token,user})
-                  }else{
+    login = async (req, res, next) => {
+        try {
+            const { email, password } = req.body;
+            const user = await User.findOne({ email }).exec();
+            if (user!=null) {
+                if (bcrypt.compareSync(password, user.password)) {
+                    const token = jwt.sign(
+                        {
+                            userId: user._id,
+                            expiresIn: '30d' // Example expiration time
+                        },
+                        process.env.JWT_SECRET
+                    );
+                    res.json({ token,user });
+                } else {
                     next({
-                        message:'The password is incorrect',
-                        status:404
-                    })
-                  }
-                }else{
-                    next({
-                        message:'The email is not registered',
-                        status:404
-                    })
+                        message: 'Incorrect password',
+                        status: 401 // Unauthorized
+                    });
                 }
-	    }catch(e){
-            showError(e,next)
+            } else {
+                next({
+                    message: 'Email not registered',
+                    status: 404 // Not Found
+                });
+            }
+        } catch (error) {
+            showError(error, next);
         }
-    }
+    };
 }
-export default new Authcontroller
+
+export default new AuthController();
--- index.js
+++ index.js
@@ -3,9 +3,19 @@ import {config} from "dotenv"
 import routes from "./routes/index.js"
 import mongoose from "mongoose";
 import parser from "body-parser"
+import cors from "cors"
 
 config();
 const app= express()
+// In your Express server setup
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+
 app.use(express.json())
 app.use(parser.urlencoded())
 
